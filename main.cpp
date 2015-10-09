8d82f875ec05f27cbcffc3a997ee0fc35d097264
Kojirion <kojirion@gmail.com>
2015-10-09 16:38:09 +0100

    Input and Output of single-letter variable

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#define BOOST_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-#include <boost/test/unit_test.hpp>
-#include <boost/test/included/unit_test.hpp>
+//#define BOOST_TEST_MAIN
+//#define BOOST_TEST_DYN_LINK
+//#include <boost/test/unit_test.hpp>
+//#include <boost/test/included/unit_test.hpp>
 
 #include <boost/spirit/include/qi_grammar.hpp>
 #include <boost/spirit/include/qi_rule.hpp>
@@ -12,30 +12,57 @@ using boost::spirit::qi::grammar;
 using boost::spirit::qi::rule;
 
 using boost::spirit::qi::int_;
-using boost::spirit::qi::lit;
+using boost::spirit::qi::char_;
 
 using Iterator = std::string::iterator;
 
-std::map<std::string, int> variables;
+std::map<char, int> variables;
 
-void F(int a){std::cout << a << '\n';}
+void Print(char a){std::cout << variables[a] << '\n';}
 
-struct PrintGrammar : grammar<Iterator, std::string()>
+void Input(char a){std::cin >> variables[a]; }
+
+struct PrintGrammar : grammar<Iterator>
 {
     PrintGrammar(): base_type(start){
-        start = "PRINT " >> int_[F];
+        start = "PRINT " >> char_[Print];
     }
 
-    rule<Iterator, std::string()> start;
+    rule<Iterator> start;
 };
 
-BOOST_AUTO_TEST_CASE(EventName)
+struct InputGrammar : grammar<Iterator>
 {
-    PrintGrammar grammar;
+    InputGrammar(): base_type(start){
+        start = "INPUT " >> char_[Input];
+    }
 
-    std::string given("PRINT 5");
+    rule<Iterator> start;
+};
 
-    bool success = boost::spirit::qi::parse(given.begin(), given.end(), grammar);
+//BOOST_AUTO_TEST_CASE(EventName)
+//{
+//    PrintGrammar grammar;
 
-    BOOST_CHECK(success);
+//    variables['a'] = 9;
+
+//    std::string given("PRINT a");
+
+//    bool success = boost::spirit::qi::parse(given.begin(), given.end(), grammar);
+
+//    BOOST_CHECK(success);
+//}
+
+int main()
+{
+    PrintGrammar printGrammar;
+    InputGrammar inputGrammar;
+
+    std::string line_1("INPUT a");
+
+    std::string line_2("PRINT a");
+
+    bool success = boost::spirit::qi::parse(line_1.begin(), line_1.end(), inputGrammar);
+
+    success = boost::spirit::qi::parse(line_2.begin(), line_2.end(), printGrammar);
 }
