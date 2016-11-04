d8504346ed4520a68df08822590c671d687913e2
Kojirion <kojirion@gmail.com>
2016-11-04 15:13:46 +0100

    Add program options

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -1,3 +1,4 @@
+#include <boost/program_options.hpp>
 #include "Grammars.hpp"
 #include <map>
 #include <vector>
@@ -22,7 +23,43 @@ struct Interpreter
 
 //TODO: a quit grammar
 
-int main(){
+int main(int ac, char* av[]){
+
+    namespace po = boost::program_options;
+
+    try {
+        po::options_description desc("Allowed options");
+        desc.add_options()
+                ("help", "Display this information")
+                ("version", "Display version information")
+                ("text", po::value<std::string>(), "Text to interpret")
+                ("file", po::value<std::string>(), "File to interpret")
+                ;
+
+        po::positional_options_description p;
+
+        po::variables_map vm;
+        po::store(po::command_line_parser(ac, av).options(desc).positional(p).run(), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            std::cout << desc << std::endl;
+            return 0;
+        }
+
+        if (vm.count("version")) {
+            std::cout << "SFBasic 0.01" << std::endl
+                      << "Copyright (C) 2016 Tina Winter\n"
+                      << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n"
+                      << "This is free software: you are free to change and redistribute it.\n"
+                      << "There is NO WARRANTY, to the extent permitted by law." << std::endl;
+            return 0;
+        }
+    } catch(std::exception& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+
     PrintGrammar printGrammar;
     InputGrammar inputGrammar;
     AdditionGrammar additionGrammar;
