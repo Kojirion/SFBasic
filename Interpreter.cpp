6d059bc07b7fe790070ff498b3e5c5aaa16ef594
Kojirion <kojirion@gmail.com>
2016-11-05 23:45:34 +0100

    Parse and handle a single DO...WhiLE construct

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -29,9 +29,9 @@ struct Interpreter
         auto it = line.begin();
         using std::placeholders::_1;
         auto r = qi::parse(it, line.end(),
-                              inputGrammar[std::bind(&Interpreter::Input, this, _1)] |
-                              printGrammar[std::bind(&Interpreter::Print, this, _1)] |
-                              additionGrammar[std::bind(&Interpreter::Add, this, _1)]);
+                           inputGrammar[std::bind(&Interpreter::Input, this, _1)] |
+                printGrammar[std::bind(&Interpreter::Print, this, _1)] |
+                additionGrammar[std::bind(&Interpreter::Add, this, _1)]);
 
         if (!r){
             std::cout << "Error at \n" << std::string(it, line.end()) << std::endl;
@@ -43,6 +43,21 @@ struct Interpreter
             std::cout << "basic> " << std::flush;
 
         for (std::string line; std::getline(is, line);){
+            qi::rule<Iterator> do_rule = "DO";
+            qi::rule<Iterator> loop_rule = "LOOP";
+            if (parse(line.begin(), line.end(), do_rule)){
+                std::getline(is, line);
+                std::vector<std::string> body;
+                while (!parse(line.begin(), line.end(), loop_rule)){
+                    body.push_back(line);
+                    std::getline(is, line);
+                }
+                while (true){ //condition goes here
+                    for (auto bodyline : body)
+                        interpretLine(bodyline);
+                }
+                continue;
+            }
             interpretLine(line);
             if (displayPrompt)
                 std::cout << "basic> " << std::flush;
@@ -100,7 +115,7 @@ int main(int ac, char* av[]){
 
         if (vm.count("file")){
             auto filename = vm["file"].as<std::string>();
-            std::ifstream file(filename);            
+            std::ifstream file(filename);
             interpreter.interpret(file, false);
             return 0;
         }
---
+++ sample3.bas
@@ -0,0 +1,3 @@
+DO
+    PRINT c
+LOOP
